624ee8fbe65b9c35706cea594c28f3771a7de569
noelevans <noelevans@gmail.com>
2015-06-04 10:11:27 +0100

    Processing for small file to add length and angle information

--- kaggle/taxi-trajectory/vectorise.py
+++ kaggle/taxi-trajectory/vectorise.py
@@ -1,30 +1,50 @@
 import math
 import numpy as np
+import re
 
 
 def dotproduct(v1, v2):
   return sum((a*b) for a, b in zip(v1, v2))
 
+
 def length(v):
   return math.sqrt(dotproduct(v, v))
+
 
 def angle(v1, v2):
   return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
 
-def line_addition(line):
-    gps = line.split(",")[-1]
 
-points = [(0,    0), 
-          (2.1,  0.1), 
-          (2.9,  0.05), 
-          (2.85, 2.2), 
-          (3.05, 4.1), 
-          (2.9,  5.9)]
+def vectors(line):
+    floats = lambda m: [float(n) for n in m.split(',')]
+    gps = [floats(m) for m in re.findall('[\d\.-]+,[\d\.-]+', line)]
 
-next_points  = points[1:]
-vecs = [np.array(n) - np.array(p) for p, n in zip(points, next_points)]
+    next_gps  = gps[1:]
+    return [np.array(n) - np.array(p) for p, n in zip(gps, next_gps)]
 
-next_vecs = vecs[1:]
-angles = [angle(v, n) for v, n in zip(vecs, next_vecs)]
 
-print vecs, angles    
+def lengths(vecs):
+    return [length(v) for v in vecs]
+
+
+def angles(vecs):
+    next_vecs = vecs[1:]
+    return [angle(v, n) for v, n in zip(vecs, next_vecs)]
+
+
+filename     = 'train.small.csv'
+new_filename = 'train.extra.small.csv'
+
+with open(new_filename, 'w') as f:
+    for n, line in enumerate(open(filename)):
+        if n == 0:
+            print 'a'
+            print line 
+            print 'a'
+            new_line = line
+        else:
+            vecs = vectors(line)
+            lengths_ = '"' + str(lengths(vecs)) + '"'
+            angles_ = '"' + str(angles(vecs)) + '"'
+            new_line = line + ',' +  lengths_ + ',' + angles_ + '\n'
+        f.write(new_line)
