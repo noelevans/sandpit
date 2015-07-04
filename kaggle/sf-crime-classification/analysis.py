c591a47071368c272c97e75cb129464d879cf730
noelevans <noelevans@gmail.com>
2015-07-04 09:37:32 +0100

    Fixing corrupt file issue in last commit. Working correctly for subset-data run

--- kaggle/sf-crime-classification/analysis.py
+++ kaggle/sf-crime-classification/analysis.py
@@ -60,63 +60,59 @@ def feature_engineering(filename, is_training=False, encoders=None):
     return df, encoders
 
 
-def main():
-    parser = optparse.OptionParser()
+parser = optparse.OptionParser()
 
-    parser.add_option('--full', action='store_true', default=False)
-    options, _ = parser.parse_args()
+parser.add_option('--full', action='store_true', default=False)
+options, _ = parser.parse_args()
 
-    if options.full:
-        print "Running full execution of training/test data"
-        train_filename = "train.csv"
-        test_filename = "test.csv"
-        independent_vars = ['DayOfWeek', 'Date', 'Time', 'X', 'Y']
-        n_trees = 2
-    else:
-        print "Running with a subset of training/test data"
-        train_filename = "train.small.csv"
-        test_filename = "test.small.csv"
-        independent_vars = ['DayOfWeek']
-        n_trees = 1
-
-
-    train, encoders = feature_engineering(train_filename, is_training=True)
-    test, _ = feature_engineering(test_filename, encoders=encoders)
-
-    districts = set(train['PdDistrict'])
-    district_ys = {}
-
-    # Grow forest and predict separately for each district's records
-    for d in districts:
-        district_name = encoders['PdDistrict'].inverse_transform(d)
-        district_train = train[train['PdDistrict'] == d]
-        district_train = district_train.drop(['PdDistrict'], axis=1)
-        district_test = test[test['PdDistrict'] == d]
-        district_test = district_test.drop(['PdDistrict'], axis=1)
-        print "Growing forest for", district_name
-
-        # Not saving output in Git so make this deterministic with random_state
-        rf = RandomForestClassifier(n_estimators=n_trees, n_jobs=-1,
-                                    random_state=782629)
-        rf.fit(district_train[independent_vars],
-               list(district_train['Category']))
-
-        district_ys[d] = rf.predict(district_test[independent_vars])
-        print "Finished", district_name
-
-    print "All predictions made"
-
-    ys = []
-    for i, row in test.iterrows():
-        d_ys = district_ys[row['PdDistrict']]
-        ys.append(d_ys.pop(0))
-
-    # Build output CSV format
-    true_indicies = [OUT_COL_NAMES.index(y) for y in ys]
-    result_row = lambda pos: [int(i == pos) for i in range((OUT_COL_NAMES))]
-    out_df = pd.DataFrame([result_row(i) for i in true_indicies])
-    out_df.to_csv('submission.csv', header=OUT_COL_NAMES, index_label='Id')
+if options.full:
+    print "Running full execution of training/test data"
+    train_filename = "train.csv"
+    test_filename = "test.csv"
+    independent_vars = ['DayOfWeek', 'Date', 'Time', 'X', 'Y']
+    n_trees = 2
+else:
+    print "Running with a subset of training/test data"
+    train_filename = "train.small.csv"
+    test_filename = "test.small.csv"
+    independent_vars = ['DayOfWeek']   #, 'Date', 'Time', 'X', 'Y']  # ['DayOfWeek']
+    n_trees = 1
 
 
-if __name__ == '__main__':
-    main()
+train, encoders = feature_engineering(train_filename, is_training=True)
+test, _ = feature_engineering(test_filename, encoders=encoders)
+
+districts = set(train['PdDistrict'])
+district_ys = {}
+
+# Grow forest and predict separately for each district's records
+for d in districts:
+    district_name = encoders['PdDistrict'].inverse_transform(d)
+    district_train = train[train['PdDistrict'] == d]
+    district_train = district_train.drop(['PdDistrict'], axis=1)
+    district_test = test[test['PdDistrict'] == d]
+    district_test = district_test.drop(['PdDistrict'], axis=1)
+    print "Growing forest for", district_name
+
+    # Not saving output in Git so make this deterministic with random_state
+    rf = RandomForestClassifier(n_estimators=n_trees, n_jobs=-1,
+                                random_state=782629)
+    rf.fit(district_train[independent_vars],
+           list(district_train['Category']))
+
+    district_ys[d] = list(rf.predict(district_test[independent_vars]))
+    print "Finished", district_name
+
+print "All predictions made"
+
+ys = []
+for i, row in test.iterrows():
+    d_ys = district_ys[row['PdDistrict']]
+    ys.append(d_ys.pop(0))
+
+# Build output CSV format
+true_indicies = [OUT_COL_NAMES.index(y) for y in ys]
+result_row = lambda pos: [int(i == pos) for i in range(len(OUT_COL_NAMES))]
+out_df = pd.DataFrame([result_row(i) for i in true_indicies])
+out_df.to_csv('submission.csv', header=OUT_COL_NAMES, index_label='Id')
+
