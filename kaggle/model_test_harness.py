698d2fca9e9b94cd3486da1d94e59e00689974df
noelevans <noelevans@gmail.com>
2015-07-08 08:47:32 +0100

    Harness to test models when they have consistent function signatures

---
+++ kaggle/model_test_harness.py
@@ -0,0 +1,36 @@
+import optparse
+import sys
+
+from sklearn.cross_validation import train_test_split
+
+
+def main():
+    parser = optparse.OptionParser()
+
+    parser.add_option('--module', action='store')
+    parser.add_option('--training', action='store')
+    options, args = parser.parse_args()
+    if not options.module:
+        print 'Usage: python model_test_harness.py --module path/to/model.py'
+        return
+    module_name = options.module.replace('.py', '').replace('/', '.')
+    training_filename = options.training or \
+            options.module[:options.module.rfind('/')] + '/train.csv'
+
+    try:
+        module_package = __import__(module_name)
+    except:
+        msg = 'Unable to import "%(m)s". Investigate with interpreter, '
+        msg = msg + 'running "__import__(%(m)s)" to investigate'
+        raise Exception(msg  % {'m': module_name})
+
+    model_module = getattr(module_package, module_name.split('.')[-1])
+    Xs, y = model_module.load_training(training_filename)
+    X_train, X_test, y_train, y_test = train_test_split(Xs, y, test_size=0.25)
+    y_hat = model_module.model_and_predict(X_train, y_train, X_test)
+
+    print sum(y_hat == y_test) / float(len(y_hat))
+
+
+if __name__ == '__main__':
+    main()
