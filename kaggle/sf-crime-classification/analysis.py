c1f1324cd28da2fe0685c07106de74841fda5fe1
noelevans <noelevans@gmail.com>
2015-07-09 21:32:45 +0100

    Reworking to fit with the model test harness

--- kaggle/sf-crime-classification/analysis.py
+++ kaggle/sf-crime-classification/analysis.py
@@ -1,5 +1,4 @@
 import optparse
-import datetime
 
 import pandas as pd
 from sklearn import preprocessing
@@ -23,12 +22,6 @@ OUT_COL_NAMES = ("ARSON", "ASSAULT", "BAD CHECKS", "BRIBERY", "BURGLARY",
                  "STOLEN PROPERTY", "SUICIDE", "SUSPICIOUS OCC", "TREA",
                  "TRESPASS", "VANDALISM", "VEHICLE THEFT", "WARRANTS",
                  "WEAPON LAWS")
-
-
-def time_diff(start, end):
-    dt_start = datetime.datetime.combine(datetime.date.today(), start)
-    dt_end = datetime.datetime.combine(datetime.date.today(), end)
-    return float((dt_end - dt_start).seconds) / (60 * 60)
 
 
 def feature_engineering(filename, is_training=False, encoders=None):
@@ -75,58 +68,82 @@ def feature_engineering(filename, is_training=False, encoders=None):
     return df, encoders
 
 
-parser = optparse.OptionParser()
+class RandomForestModel(KaggleModel):
 
-parser.add_option('--full', action='store_true', default=False)
-options, _ = parser.parse_args()
+    def __init__(self, full_run=False):
+        if full_run:
+            self.n_trees = 60
+        else:
+            self.n_trees = 1
 
-if options.full:
-    print "Running full execution of training/test data"
-    train_filename = "train.csv"
-    test_filename = "test.csv"
-    independent_vars = ['DayOfWeek', 'Date', 'Time', 'X', 'Y']
-    n_trees = 1
-else:
-    print "Running with a subset of training/test data"
-    train_filename = "train.small.csv"
-    test_filename = "test.small.csv"
-    independent_vars = ['DayOfWeek', 'Date', 'Time', 'X', 'Y']
-    n_trees = 1
+    def load_training(training_filename):
+        ''' For use with model_test_harness '''
+        training, _ = feature_engineering(training_filename, is_training=True)
+        self.columns = list(training.ix[:,1:].columns)
+        return training.ix[:,1:], training.ix[:,0]
 
 
-train, encoders = feature_engineering(train_filename, is_training=True)
-test, _ = feature_engineering(test_filename, encoders=encoders)
+    def model_and_predict(X_train, y_train, X_test):
+        district_idx = self.columns.index('PdDistrict')
+        districts = set(X_train[[district_idx]])
+        district_ys = {}
+        # Grow forest and predict separately for each district's records
+        for d in districts:
+            district_train = X_train[X_train[[district_idx]] == d]
+            district_train = district_train.drop(['PdDistrict'], axis=1)
+            district_test = X_test[X_test['PdDistrict'] == d]
+            district_test = district_test.drop(['PdDistrict'], axis=1)
+            print "Growing forest for", d
 
-districts = set(train['PdDistrict'])
-district_ys = {}
+            # Not saving output in Git so make this deterministic with random_state
+            rf = RandomForestClassifier(n_estimators=self.n_trees, n_jobs=-1,
+                                        random_state=782629)
+            rf.fit(district_train[independent_vars], list(y_train))
 
-# Grow forest and predict separately for each district's records
-for d in districts:
-    district_name = encoders['PdDistrict'].inverse_transform(d)
-    district_train = train[train['PdDistrict'] == d]
-    district_train = district_train.drop(['PdDistrict'], axis=1)
-    district_test = test[test['PdDistrict'] == d]
-    district_test = district_test.drop(['PdDistrict'], axis=1)
-    print "Growing forest for", district_name
+            district_ys[d] = list(rf.predict(district_test[independent_vars]))
+            print "Finished", d
 
-    # Not saving output in Git so make this deterministic with random_state
-    rf = RandomForestClassifier(n_estimators=n_trees, n_jobs=-1,
-                                random_state=782629)
-    rf.fit(district_train[independent_vars],
-           list(district_train['Category']))
+        print "All predictions made"
 
-    district_ys[d] = list(rf.predict(district_test[independent_vars]))
-    print "Finished", district_name
+        y_hat = []
+        for i, row in X_test.iterrows():
+            d_ys = district_ys[row['PdDistrict']]
+            y_hat.append(d_ys.pop(0))
 
-print "All predictions made"
+        return y_hat
 
-ys = []
-for i, row in test.iterrows():
-    d_ys = district_ys[row['PdDistrict']]
-    ys.append(d_ys.pop(0))
 
-# Build output CSV format
-true_indicies = [OUT_COL_NAMES.index(y) for y in ys]
-result_row = lambda pos: [int(i == pos) for i in range(len(OUT_COL_NAMES))]
-out_df = pd.DataFrame([result_row(i) for i in true_indicies])
-out_df.to_csv('submission.csv', header=OUT_COL_NAMES, index_label='Id')
+def main():
+    parser = optparse.OptionParser()
+
+    parser.add_option('--full', action='store_true', default=False)
+    options, _ = parser.parse_args()
+
+    if options.full:
+        print "Running full execution of training/test data"
+        train_filename = "train.csv"
+        test_filename = "test.csv"
+        independent_vars = ['DayOfWeek', 'Date', 'Time', 'X', 'Y']
+        n_trees = 1
+    else:
+        print "Running with a subset of training/test data"
+        train_filename = "train.small.csv"
+        test_filename = "test.small.csv"
+        independent_vars = ['DayOfWeek', 'Date', 'Time', 'X', 'Y']
+        n_trees = 1
+
+
+    train, encoders = feature_engineering(train_filename, is_training=True)
+    test, _ = feature_engineering(test_filename, encoders=encoders)
+    
+    ys = model_and_predict(training.ix[:,1:], training.ix[:,0])
+
+    # Build output CSV format
+    true_indicies = [OUT_COL_NAMES.index(y) for y in ys]
+    result_row = lambda pos: [int(i == pos) for i in range(len(OUT_COL_NAMES))]
+    out_df = pd.DataFrame([result_row(i) for i in true_indicies])
+    out_df.to_csv('submission.csv', header=OUT_COL_NAMES, index_label='Id')
+
+
+if __name__ == '__main__':
+    main()
