59976b3a0764088700f3ce3a90f9ac4cd25d6a0d
noelevans <noelevans@gmail.com>
2015-08-25 20:55:27 +0100

    Alias ipython to run this on startup each time

---
+++ start_ipython.py
@@ -0,0 +1,16 @@
+import datetime
+import decimal
+import math
+import operator
+import random
+import re
+
+import numpy as np
+import pandas as pd
+
+
+pd.set_option('display.max_columns', 30)
+pd.set_option('display.width', 100)
+
+df_small = pd.DataFrame([[1,2,3], [2,4,8]], columns=['a', 'b', 'c'])
+
