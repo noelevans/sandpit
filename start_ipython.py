c41a83c6e2d70023964d887f3b40d61f86156d89
noelevans <noelevans@gmail.com>
2015-08-29 21:09:40 +0100

    Gone through majority of code to find most frequently used import statements

--- start_ipython.py
+++ start_ipython.py
@@ -1,12 +1,27 @@
+"""
+    To be used with ipython when it starts up. Create a sym-link to this file in
+    the default ipython profile like so:
+        ln -s ~/repo/playground/start_ipython.py
+                ~/.ipython/profile_default/startup/start_ipython.py
+"""
+
 import datetime
 import decimal
+import itertools
 import math
 import operator
 import random
 import re
+import timeit
 
+# import matplotlib.pyplot as plt    # very slow
 import numpy as np
 import pandas as pd
+import scipy as sp
+
+from collections import Counter
+from pprint import pprint as pp
+from sklearn import datasets
 
 
 pd.set_option('display.max_columns', 30)
