ce202d7a23808680772e73e322d1a6ab4a6397d0
noelevans <noelevans@gmail.com>
2015-09-13 21:53:59 +0100

    Two independents with uniform random distribution with 2 and 3D representations

---
+++ bayesian_methods_for_hackers/uniform_ch03.py
@@ -0,0 +1,37 @@
+import numpy as np
+import scipy.stats as stats
+
+from IPython.core.pylabtools import figsize
+
+
+def main():
+    figsize(12.5, 4)
+
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+
+    jet = plt.cm.jet
+    fig = plt.figure()
+    x = y = np.linspace(0, 5, 100)
+    X, Y = np.meshgrid(x, y)
+
+    plt.subplot(121)
+    uni_x = stats.uniform.pdf(x, loc=0, scale=5)
+    uni_y = stats.uniform.pdf(y, loc=0, scale=5)
+    M = np.dot(uni_x[:, None], uni_y[None, :])
+    im = plt.imshow(M, interpolation='none', origin='lower',
+                    cmap=jet, vmax=1, vmin=-.15, extent=(0, 5, 0, 5))
+
+    plt.xlim(0, 5)
+    plt.ylim(0, 5)
+    plt.title("Landscape formed by Uniform priors.")
+
+    ax = fig.add_subplot(122, projection='3d')
+    ax.plot_surface(X, Y, M, cmap=plt.cm.jet, vmax=1, vmin=-.15)
+    ax.view_init(azim=390)
+    plt.title("Uniform prior landscape; alternate view")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
