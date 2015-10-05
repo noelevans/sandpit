8feca68a3aab89bd2ecc9fbf092bb05bab124231
noelevans <noelevans@gmail.com>
2015-10-05 21:07:39 +0100

    Change of repo name. Update effected paths

--- bayesian_methods_for_hackers/changepoints_ch02.py
+++ bayesian_methods_for_hackers/changepoints_ch02.py
@@ -7,9 +7,6 @@ import pymc as pm
 
 
 def main():
-    matplotlibrc_path = '/home/noel/repo/playground/matplotlibrc.json'
-    matplotlib.rcParams.update(json.load(open(matplotlibrc_path)))
-
     lambda_1 = pm.Exponential("lambda_1", 1)  # prior on first behaviour
     lambda_2 = pm.Exponential("lambda_2", 1)  # prior on second behaviour
     tau = pm.DiscreteUniform("tau", lower=0, upper=10)  # prior on behaviour change
--- start_ipython.py
+++ start_ipython.py
@@ -1,7 +1,7 @@
 """
     To be used with ipython when it starts up. Create a sym-link to this file in
     the default ipython profile like so:
-        ln -s ~/repo/playground/start_ipython.py
+        ln -s ~/repo/sandpit/start_ipython.py
                 ~/.ipython/profile_default/startup/start_ipython.py
 """
 
@@ -26,9 +26,9 @@ import scipy as sp
 from collections import Counter
 from pprint import pprint as pp
 from sklearn import datasets
+from scipy import stats
 
 from IPython.core.pylabtools import figsize
-
 
 
 pd.set_option('display.max_columns', 30)
@@ -43,8 +43,12 @@ def nCr(n, r):
     return sp.misc.comb(n, r, exact=True)
 
 
-matplotlibrc_path = '/home/noel/repo/playground/matplotlibrc.json'
-matplotlib.rcParams.update(json.load(open(matplotlibrc_path)))
+def exp_decay(days, half_life=0.05):
+    return half_life ** (days / 30.0)
+
+
+# matplotlibrc_path = '/home/noel/repo/sandpit/matplotlibrc.json'
+# matplotlib.rcParams.update(json.load(open(matplotlibrc_path)))
 
 df_small = pd.DataFrame([[1,2,3], [2,4,8]], columns=['a', 'b', 'c'])
 
