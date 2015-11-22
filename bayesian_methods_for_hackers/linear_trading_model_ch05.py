04511c20121cef52935b99d941abc814ea8b0289
noelevans <noelevans@gmail.com>
2015-11-22 22:04:34 +0000

    Find best fit coefficients with pymc

--- bayesian_methods_for_hackers/linear_trading_model_ch05.py
+++ bayesian_methods_for_hackers/linear_trading_model_ch05.py
@@ -1,5 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import pymc as pm
+from pymc.Matplot import plot as mcplot
 
 
 def main():
@@ -20,6 +22,26 @@ def main():
     plt.ylim(Y.min(), Y.max())
     plt.legend(loc="upper left")
     plt.show()
+
+    std = pm.Uniform("std", 0, 100, trace=False)
+
+    @pm.deterministic
+    def prec(U=std):
+        return 1.0 / (U) ** 2
+
+    beta = pm.Normal("beta", 0, 0.0001)
+    alpha = pm.Normal("alpha", 0, 0.0001)
+
+
+    @pm.deterministic
+    def mean(X=X, alpha=alpha, beta=beta):
+        return alpha + beta * X
+
+    obs = pm.Normal("obs", mean, prec, value=Y, observed=True)
+    mcmc = pm.MCMC([obs, beta, alpha, std, prec])
+
+    mcmc.sample(100000, 80000)
+    mcplot(mcmc)
 
 
 if __name__ == '__main__':
