c6f9117c376d3a524589f8ad0b4bbbe221708d19
noelevans <noelevans@gmail.com>
2015-12-14 23:08:50 +0000

    Waterloo and City line never runs on weekend so stop false-positive alert

--- rpi/tube_status.py
+++ rpi/tube_status.py
@@ -2,33 +2,37 @@ import requests
 from piglow import PiGlow
 
 
-URL = 'http://api.tfl.gov.uk/Line/Mode/tube/Status'
-requests.packages.urllib3.disable_warnings()
-
-
 def update():
-    resp = requests.get(URL)
-    ol = resp.json()
+    requests.packages.urllib3.disable_warnings()
+    resp = requests.get('http://api.tfl.gov.uk/Line/Mode/tube/Status').json()
 
     statuses = {el['id']: el['lineStatuses'][0]['statusSeverityDescription']
-                for el in ol}
+                for el in resp}
     return {k: statuses[k] == 'Good Service' for k in statuses.keys()}
 
 
 def main():
     piglow = PiGlow()
+    piglow.all(0)
     running = update()
+    important_lines_running = True
 
     if running.pop('metropolitan'):
         piglow.led1(1)
     else:
         piglow.arm1(1)
+        important_lines_running = False
 
     if running.pop('jubilee'):
         piglow.led7(1)
         piglow.led8(1)
     else:
         piglow.arm2(1)
+        important_lines_running = False
+
+    # Waterloo and City line never runs on the weekend
+    if datetime.date.today().isoweekday() in (6, 7):
+        running.pop('waterloo-city')
 
     if all(running.values()):
         piglow.led13(1)
@@ -36,7 +40,9 @@ def main():
         piglow.led15(1)
     else:
         piglow.arm3(1)
-        piglow.led18(0)
+        if important_lines_running:
+            # asthetic tweak to avoid bright white LED always being on "alone"
+            piglow.led18(0)
 
 
 if __name__ == '__main__':
