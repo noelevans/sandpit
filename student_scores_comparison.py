aa42d708152026cc8dcc41d8bc4e719382a0d902
noelevans <noelevans@gmail.com>
2016-06-10 20:28:25 +0100

    Compare two scenarios of scoring where the scores have very low variance in one and a wider variance in the other

--- student_scores_comparison.py
+++ student_scores_comparison.py
@@ -15,25 +15,27 @@ import numpy as np
 
 """
 
-STUDENT_1 = [84, 72, 57, 46, 63, 76, 99, 91]
-STUDENT_2 = [81, 69, 74, 61, 56, 87, 69, 65, 66, 44, 62, 69]
+SCORES_1 = np.array([84, 72, 57, 46, 63, 76, 99, 91,
+                     81, 69, 74, 61, 56, 87, 69, 65, 66, 44, 62, 69])
+SCORES_2 = np.array([73, 73, 73, 73, 73, 74, 74, 74,
+                     67, 67, 67, 67, 67, 67, 67, 67, 67, 67, 67, 66])
 
-
-def trial():
-    scores = np.array(STUDENT_1 + STUDENT_2)
+def trial(scores):
     np.random.shuffle(scores)
-    score_1, score_2 = scores[:len(STUDENT_1)], scores[len(STUDENT_1):]
-    return score_1.mean() - score_2.mean()
+    student_1 = scores[:8]
+    student_2 = scores[8:]
+    return student_1.mean() - student_2.mean()
 
 
 def main():
-    scores = []
-    tests = 100000
-    for _ in range(tests):
-        scores.append(trial())
+    tests = 1000000
+    scores_1 = [trial(SCORES_1) for _ in range(tests)]
+    scores_2 = [trial(SCORES_2) for _ in range(tests)]
 
-    plt.hist(scores, 20)
+    plt.hist(scores_1, 16, color='blue', alpha=0.4)
+    plt.hist(scores_2, 16, color='red', alpha=0.3)
     plt.show()
+
 
 if __name__ == '__main__':
     main()
