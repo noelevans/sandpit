a135b5555c1592d548bdf8871a4ff646337c3622
noelevans <noelevans@gmail.com>
2016-07-19 21:14:43 +0100

    Neatening code

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,11 +1,11 @@
-import numpy as np
-
-
 """
-Implementing the challenge set here:
+Implementing Einstein's Fish riddle explained here:
     http://blog.pluszero.ca/blog/2016/07/17/
         using-simulated-annealing-to-solve-logic-puzzles/
 """
+
+import numpy as np
+
 
 # house          1            2          3          4            5
 animals       = ['bird',      'dog',     'cat',     'horse',     'fish']
@@ -18,15 +18,21 @@ houses = np.array([animals, cigarettes, nationalities, house_colours, drinks])
 
 
 def adjacents(arr):
-    return (np.arange(len(filter(None, arr))) == arr.nonzero()[0] - min(arr.nonzero()[0])).all()
+    return (np.arange(len(filter(None, arr))) ==
+            arr.nonzero()[0] - min(arr.nonzero()[0])
+           ).all()
 
 
 def cost(cfg):
-    """
+    """ Score for a configuration of the houses' wrt constraints of the riddle.
+
+    Lower result indicates a better configuration; closer to the desired constraints.
+
     The Brit lives in the house with red walls.
     The Swede has a dog.
     The Dane drinks tea.
-    The house with green walls is directly to the left of the house with white walls.
+    The house with green walls is directly to the left of the house with white
+        walls.
     The owner of the house with green walls drinks coffee.
     The person who smokes Pall Mall cigars owns a bird.
     The owner of the house with yellow walls smokes Dunhill.
@@ -39,43 +45,28 @@ def cost(cfg):
     The Norwegian lives next to the house with blue walls.
     The man who smokes Blends lives next to the man who drinks water.
     """
-    price = 0
-    if 2 in ((cfg == 'british') | (cfg == 'red')).sum(axis=0):
-        price -= 1
-    if 2 in ((cfg == 'swedish') | (cfg == 'dog')).sum(axis=0):
-        price -= 1
-    if 2 in ((cfg == 'danish') | (cfg == 'tea')).sum(axis=0):
-        price -= 1
-    if list(cfg[3,:]).index('green') - list(cfg[3,:]).index('white') == 1:
-        price -= 1
-    if 2 in ((cfg == 'green') | (cfg == 'coffee')).sum(axis=0):
-        price -= 1
-    if 2 in ((cfg == 'bird') | (cfg == 'pall mall')).sum(axis=0):
-        price -= 1
-    if 2 in ((cfg == 'yellow') | (cfg == 'dunhill')).sum(axis=0):
-        price -= 1
-    if 'milk' in cfg[:, 2]:
-        price -= 1
-    if 'norwegian' in cfg[:, 0]:
-        price -= 1
-    if adjacents(((cfg == 'blends') | (cfg == 'cat')).sum(0)):
-        price -= 1
-    if adjacents(((cfg == 'horse') | (cfg == 'dunhill')).sum(0)):
-        price -= 1
-    if 2 in ((cfg == 'blue master') | (cfg == 'root beer')).sum(axis=0):
-        price -= 1
-    if 2 in ((cfg == 'german') | (cfg == 'prince')).sum(axis=0):
-        price -= 1
-    if adjacents(((cfg == 'norwegian') | (cfg == 'blue')).sum(0)):
-        price -= 1
-    if adjacents(((cfg == 'blends') | (cfg == 'water')).sum(0)):
-        price -= 1
-
-    return price
+    score = [
+        2 in ((cfg == 'british') | (cfg == 'red')).sum(axis=0),
+        2 in ((cfg == 'swedish') | (cfg == 'dog')).sum(axis=0),
+        2 in ((cfg == 'danish') | (cfg == 'tea')).sum(axis=0),
+        1 == list(cfg[3,:]).index('green') - list(cfg[3,:]).index('white'),
+        2 in ((cfg == 'green') | (cfg == 'coffee')).sum(axis=0),
+        2 in ((cfg == 'bird') | (cfg == 'pall mall')).sum(axis=0),
+        2 in ((cfg == 'yellow') | (cfg == 'dunhill')).sum(axis=0),
+        'milk' in cfg[:, 2],
+        'norwegian' in cfg[:, 0],
+        adjacents(((cfg == 'blends') | (cfg == 'cat')).sum(0)),
+        adjacents(((cfg == 'horse') | (cfg == 'dunhill')).sum(0)),
+        2 in ((cfg == 'blue master') | (cfg == 'root beer')).sum(axis=0),
+        2 in ((cfg == 'german') | (cfg == 'prince')).sum(axis=0),
+        adjacents(((cfg == 'norwegian') | (cfg == 'blue')).sum(0)),
+        adjacents(((cfg == 'blends') | (cfg == 'water')).sum(0)),
+    ]
+    return -1 * sum(score)
 
 
 def main():
-    print cost(houses)
+    print(cost(houses))
 
 
 if __name__ == '__main__':
