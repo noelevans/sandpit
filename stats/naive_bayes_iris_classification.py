64e20e30cf4f5c50666aac0294c86ab57166c3f1
noelevans <noelevans@gmail.com>
2016-11-08 20:54:47 +0000

    Separate train and evaluate sets from iris dataset for fairer test of algo

--- stats/naive_bayes_iris_classification.py
+++ stats/naive_bayes_iris_classification.py
@@ -7,15 +7,16 @@ from sklearn.cross_validation import train_test_split
 
 def main():
     iris = datasets.load_iris()
-    res = train_test_split(iris.data, iris.target, test_size=0.2)
+    res = train_test_split(iris.data, iris.target,
+        test_size=0.2, random_state=0)
     train_X, test_X, train_y, test_y = res
 
     gnb = GaussianNB()
-    model = gnb.fit(iris['data'], iris['target'])
-    y_hat = model.predict(iris['data'])
+    model = gnb.fit(train_X, train_y)
+    y_hat = model.predict(test_X)
 
-    tests = iris.data.shape[0]
-    fails = (iris.target != y_hat).sum()
+    tests = test_y.shape[0]
+    fails = (test_y != y_hat).sum()
     print('Number of mislabeled points out of a total %d points : %d' %
         (tests, fails))
     print('Cost: %.2f' % (fails / tests))
