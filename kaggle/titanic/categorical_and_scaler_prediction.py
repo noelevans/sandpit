247c1fc0af2556a5bd421488430d97f45c533771
noelevans <noelevans@gmail.com>
2016-11-30 21:42:20 +0000

    Make predictions with gender and ticket price

--- kaggle/titanic/categorical_and_scaler_prediction.py
+++ kaggle/titanic/categorical_and_scaler_prediction.py
@@ -1,10 +1,25 @@
 import pandas
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 
 def main():
     train_all = pandas.DataFrame.from_csv('train.csv')
-    train = train_all[['Survived', 'Sex', 'Fare']]
-    print(train)
+    train = train_all[['Survived', 'Sex', 'Fare']][:20]
+
+    gender_label = LabelEncoder()
+    train.Sex = gender_label.fit_transform(train.Sex)
+
+    X = train[['Sex', 'Fare']]
+    y = train['Survived']
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42)
+
+    clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+
+    print(clf.predict(X_test))
 
 
 if __name__ == '__main__':
