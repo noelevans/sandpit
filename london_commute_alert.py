e5994d5146406e190ff91e23c84daef3e82e1619
Noel Evans <noelevans@gmail.com>
2017-03-26 21:11:12 +0100

    More descriptive message about Tube problem - TFL's reason

--- london_commute_alert.py
+++ london_commute_alert.py
@@ -3,17 +3,19 @@ import os
 import requests
 
 
-def update():
+def update(lines):
     url = 'http://api.tfl.gov.uk/Line/Mode/tube/Status'
     resp = requests.get(url).json()
-    result = {}
+    result = []
 
     for el in resp:
         value = el['lineStatuses'][0]
-        if value['statusSeverityDescription'] != 'Good Service':
-            message = '{} ({})'.format(value['statusSeverityDescription'],
-                                       value['reason'])
-            result[el['id']] = message
+        state = value['statusSeverityDescription']
+        if el['id'] in lines and state != 'Good Service':
+            result.append('{}: {} ({})'.format(
+                el['id'].capitalize(),
+                value['statusSeverityDescription'],
+                value['reason']))
 
     return result
 
@@ -22,21 +24,16 @@ def email(lines):
     with open('curl_raw_command.sh') as f:
         raw_command = f.read()
 
-    subject = 'Tube delays for commute'
-    body = '\n\n'.join(': '.join([line.capitalize(), s]) for line, s in lines.items())
-
     # We must have this running on PythonAnywhere - Monday to Sunday.
     # Ignore Saturday and Sunday
     if datetime.date.today().isoweekday() in range(1, 6):
-        os.system(raw_command.format(subject=subject, body=body))
+        os.system(raw_command.format(subject='Tube delays for commute', 
+                                     body='\n\n'.join(lines)))
 
 
 def main():
     commute_lines = ['metropolitan', 'jubilee', 'central']
-    status = update()
-    print(status)
-    delays = {c: status[c] for c in commute_lines if status.get(c)}
-
+    delays = update(commute_lines)
     if delays:
         email(delays)
 
