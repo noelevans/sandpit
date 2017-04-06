61748b3245e857f37d0953bad6e6d3445884ae14
Noel Evans <noelevans@gmail.com>
2017-04-06 21:38:30 +0100

    Predict using scaler values

--- boston/categorise_with_scaler_and_category.py
+++ boston/categorise_with_scaler_and_category.py
@@ -7,25 +7,32 @@ from   sklearn.cross_validation import train_test_split
 
 def main():
     boston = datasets.load_boston()
-    y = boston.target     # House prices
+    y = boston.target       # House prices
     mean = np.mean(y)
     y = y > mean            # y now means is_above_average_house_price
 
     fns = boston.feature_names
     predictors = np.array([
-        'CHAS',     # Is district close to river
-        'NOX',      # Air concentration of nitrous-oxide
-        'CRIM',     # Crime rate per capita
+        'NOX',              # Air concentration of nitrous-oxide
+        'CRIM',             # Crime rate per capita
         ])
     X_idx = np.in1d(fns, predictors)
     X = boston.data[:, X_idx]
 
     X_train, X_test, y_train, y_test = train_test_split(
-        y, X, test_size=0.25, random_state=33)
+        X, y, test_size=0.25, random_state=33)
 
     for p, x in zip(predictors, np.rollaxis(X, 1)):
         print('%s vs House price - srcc: %f, p_value: %f' % (
             (p, ) + stats.spearmanr(x, y)))
+
+    model = GaussianNB()
+    model.fit(X_train, y_train)
+    y_hat = model.predict(X_test)
+
+    matches = y_hat == y_test
+    print('Success rate: %i / %i = %f' % (
+        matches.sum(), matches.size, float(matches.sum()) / matches.size))
 
 
 if __name__ == '__main__':
