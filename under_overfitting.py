d72556e711b290e163eb778b38b2d76666c56f49
Noel Evans <noelevans@gmail.com>
2017-05-12 21:35:37 +0100

    Show optimal degrees of polynomial identification

--- under_overfitting.py
+++ under_overfitting.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
@@ -15,6 +16,9 @@ def main():
     X = np.sort(np.random.rand(n_samples))
     y = true_fn(X) + np.random.randn(n_samples) * 0.1
 
+    mses = []
+    stds = [] 
+
     for d in degrees:
         poly_features = PolynomialFeatures(degree=d, include_bias=False)
         model = LinearRegression()
@@ -27,7 +31,21 @@ def main():
 
         print('Degree {:>2}: mse = {}, std = {}'.format(
             d, -scores.mean(), scores.std()))
+        mses.append(-scores.mean())
+        stds.append(scores.std())
     
+    fig, ax1 = plt.subplots()
+    ax2 = ax1.twinx()
+
+    ax1.plot(degrees, mses, label='MSE', color='cornflowerblue')  
+    ax2.plot(degrees, stds, label='Sigma', color='teal')
+    ax1.set_yscale('log')
+    ax2.set_yscale('log')
+    ax1.set_xlabel('Coefficients / degrees')
+    ax1.set_ylabel('Mean sq error', color='cornflowerblue')
+    ax2.set_ylabel('Std deviation', color='teal')
+    plt.title('Approximate noisy signal with varying number of degrees')
+    plt.show()    
 
 if __name__ == '__main__':
     main()
