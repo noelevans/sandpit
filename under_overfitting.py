0db9b5ab1eb2ac203b3e0128708cf009feff2b1b
Noel Evans <noelevans@gmail.com>
2017-05-13 21:05:46 +0100

    Option to show bias/overfitting error optimal point and visualisations of each option

--- under_overfitting.py
+++ under_overfitting.py
@@ -10,14 +10,14 @@ def main():
     np.random.seed(0)
     
     n_samples = 30
-    degrees = range(1, 16)
-
     true_fn = lambda X: np.cos(1.5 * np.pi * X)
     X = np.sort(np.random.rand(n_samples))
     y = true_fn(X) + np.random.randn(n_samples) * 0.1
 
     mses = []
-    stds = [] 
+    stds = []
+    pipelines = []
+    degrees = range(1, 16)
 
     for d in degrees:
         poly_features = PolynomialFeatures(degree=d, include_bias=False)
@@ -33,19 +33,41 @@ def main():
             d, -scores.mean(), scores.std()))
         mses.append(-scores.mean())
         stds.append(scores.std())
+        pipelines.append(pipeline)
     
-    fig, ax1 = plt.subplots()
-    ax2 = ax1.twinx()
+    if False:
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()
+        ax1.plot(degrees, mses, label='MSE', color='cornflowerblue')
+        ax2.plot(degrees, stds, label='Sigma', color='teal')
+        ax1.set_yscale('log')
+        ax2.set_yscale('log')
+        ax1.set_xlabel('Degrees')
+        ax1.set_ylabel('Mean sq error', color='cornflowerblue')
+        ax2.set_ylabel('Std deviation', color='teal')
+        plt.title('Approximating noisy signal varying polynomial coefficients')
+    else:
+        height = 4
+        width = 4
+        n = 0
+        fig, axs = plt.subplots(height, width)
 
-    ax1.plot(degrees, mses, label='MSE', color='cornflowerblue')  
-    ax2.plot(degrees, stds, label='Sigma', color='teal')
-    ax1.set_yscale('log')
-    ax2.set_yscale('log')
-    ax1.set_xlabel('Coefficients / degrees')
-    ax1.set_ylabel('Mean sq error', color='cornflowerblue')
-    ax2.set_ylabel('Std deviation', color='teal')
-    plt.title('Approximate noisy signal with varying number of degrees')
-    plt.show()    
+        for h in range(height):
+            for w in range(width):
+                ax = axs[h, w]
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                if h == w == 0:
+                    ax.set_title('Raw data', fontsize=10)
+                    ax.scatter(X, y, color='teal')
+                else:
+                    p = pipelines.pop(0)
+                    n += 1
+                    ax.set_title('{} degrees'.format(n), fontsize=10)
+                    ax.plot(X, p.predict(X[:, np.newaxis]), color='teal')
+        plt.suptitle('Plots varying degrees of coefficients for independent')
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
