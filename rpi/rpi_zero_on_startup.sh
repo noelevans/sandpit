7170e5492ae8b347ad230ea2bf4c89e9d58ed54e
Noel Evans <noelevans@gmail.com>
2018-01-16 15:18:43 +0000

    Startup tools for RPi zero

---
+++ rpi/rpi_zero_on_startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This is to be used in conjunction with 
+# sandpit/rpi/RPi-zero-startup.desktop
+# Copy RPi-zero-startup.desktop to /etc/xdg/autostart/
+# and copy this script to /home/pi/rpi_zero_on_startup.sh
+
+/usr/bin/calibre &
+
