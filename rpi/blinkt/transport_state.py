0a32b3f793b978b7f5b723fe8cfb51773b3721db
Noel Evans <noelevans@gmail.com>
2018-02-03 15:20:12 +0000

    Control pixels with flashing as appropriate

--- rpi/blinkt/transport_state.py
+++ rpi/blinkt/transport_state.py
@@ -8,6 +8,7 @@ Handle these cases carefully:
 """
 
 import colours
+import time
 
 
 def typo_correct(input):
@@ -51,13 +52,44 @@ def transport_status():
     return {k: status_aliases.get(statuses[k], 'BAD') for k in statuses.keys()}
 
 
+def pixel_operation(status):
+
+    def good_op(x, r, g, b):
+        blinkt.set_pixel(x, r, g, b, brightness=0.2)
+
+    def flash_op(x, r, g, b):
+        while True:
+            blinkt.set_pixel(x, r, g, b, brightness=0.2)
+            time.sleep(1)
+            blinkt.set_pixel(x, r, g, b, brightness=0.0)
+            time.sleep(1)
+
+    def off_op(x, r, g, b):
+        blinkt.set_pixel(x, r, g, b, brightness=0.0)
+
+    return {
+            'GOOD': good_op,
+            'OK':   flash_op,
+            'BAD':  off_op,
+        }[status]
+
+
+def set_pixel(n, rgb, fn):
+    fn(n, *rgb)
+
+
 def illuminate():
     lines = line_choices()
     status = transport_status()
     line_colours = [colours.LINE_COLOURS.get(el) for el in lines]
 
-    # for n in range(8):
-    #     blinkt.set_pixel(x, r, g, b, brightness=None)
+    for n, (rgb, s) in enumerate(zip(line_colours, status)):
+        set_pixel(n, rgb, pixel_operation(s))
+
+
+def main():
+    illuminate()
+    time.sleep(10)
 
 
 if __name__ == '__main__':
