6c3733719822631c2e355da7253ec4ec21cc6b8b
Noel Evans <noelevans@gmail.com>
2019-02-14 10:13:27 +0000

    Ticker challenge and reordering for easy work of test against fn code

--- koans/answer_template.py
+++ koans/answer_template.py
@@ -7,6 +7,7 @@ or
 
 '''
 
+import mendeleev
 import pytest
 
 
@@ -15,9 +16,17 @@ def inclusive_range(n):
     return [1, 2, 3, 4, 5]
 
 
+def test_inclusive_range():
+    assert list(inclusive_range(5)) == [1, 2, 3, 4, 5]
+
+
 def average(ol):
     # The mean for a series of numbers
     pass
+
+
+# def test_average():
+#     assert average([2, 2, 2, 3, 4]) == 2.6
 
 
 def no_whitespace(t):
@@ -25,9 +34,17 @@ def no_whitespace(t):
     pass
 
 
+# def test_no_whitespace():
+#     assert no_whitespace('   hello    ') == 'hello'
+
+
 def minus_to_plus(t):
     # Replace all - symbols with + characters
     pass
+
+
+# def test_minus_to_plus():
+#     assert minus_to_plus('hello-world') == 'hello+world'
 
 
 def sum_bar_last(a, b, c):
@@ -35,14 +52,26 @@ def sum_bar_last(a, b, c):
     pass
 
 
+# def test_sum_bar_last():
+#     assert sum_bar_last(4, 5, 6) == 9
+
+
 def mirror(ol):
     # Turn a string around so 1st char is the last, 2nd becomes penultimate
     pass
 
 
+# def test_mirror():
+#     assert mirror('smart') == 'trams'
+
+
 def odd_idxs(ol):
     # For ol = [4, 5, 6, 7, 8, 9]  return [5, 7, 9] the odd indicies of ol
     pass
+
+
+# def test_odd_idxs():
+#     assert odd_idxs([1, 2, 3, 4]) == [2, 4]
 
 
 def pairs(ol):
@@ -51,9 +80,17 @@ def pairs(ol):
     pass
 
 
+# def test_pairs():
+#     assert pairs([0, 1, 2, 3, 4, 5]) == [[0, 1], [2, 3], [4, 5]]
+
+
 def blank_count(ol):
     # Count the amount of not True elements in ol
     pass
+
+
+# def test_blank_count():
+#     assert blank_count([7, 0, None, 1, 'hi', '', 88, 0]) == 4
 
 
 def flatten(ol_of_ol):
@@ -61,64 +98,13 @@ def flatten(ol_of_ol):
     pass
 
 
+# def test_flatten():
+#     assert flatten([[1, 2, 3], [4, 5], [6, 7]]) == [1, 2, 3, 4, 5, 6, 7]
+
+
 def element_divisible_by_3(ol):
     # Is there 1 or more elements divisible by 3 in the input
     pass
-
-
-def most_common(ol):
-    # Return the most common element in the input list
-    pass
-
-
-def dict_reverse(d):
-    # For {'a': 3, 'b': 4, 'c': 9} -> {3: 'a', 4: 'b', 9: 'c'}
-    pass
-
-
-###############################################################################
-# Validation
-###############################################################################
-
-
-def test_inclusive_range():
-    assert list(inclusive_range(5)) == [1, 2, 3, 4, 5]
-
-
-# def test_average():
-#     assert average([2, 2, 2, 3, 4]) == 2.6
-
-
-# def test_no_whitespace():
-#     assert no_whitespace('   hello    ') == 'hello'
-
-
-# def test_minus_to_plus():
-#     assert minus_to_plus('hello-world') == 'hello+world'
-
-
-# def test_sum_bar_last():
-#     assert sum_bar_last(4, 5, 6) == 9
-
-
-# def test_mirror():
-#     assert mirror('smart') == 'trams'
-
-
-# def test_odd_idxs():
-#     assert odd_idxs([1, 2, 3, 4]) == [2, 4]
-
-
-# def test_pairs():
-#     assert pairs([0, 1, 2, 3, 4, 5]) == [[0, 1], [2, 3], [4, 5]]
-
-
-# def test_blank_count():
-#     assert blank_count([7, 0, None, 1, 'hi', '', 88, 0]) == 4
-
-
-# def test_flatten():
-#     assert flatten([[1, 2, 3], [4, 5], [6, 7]]) == [1, 2, 3, 4, 5, 6, 7]
 
 
 # def test_element_divisible_by_3():
@@ -126,12 +112,86 @@ def test_inclusive_range():
 #     assert element_divisible_by_3([1, 2, 6, 5]) == True
 
 
+def most_common(ol):
+    # Return the most common element in the input list
+    pass
+
+
 # def test_most_common():
 #     assert most_common([3, 3, 4, 4, 4, 4, 2]) == 4
 
 
+def dict_reverse(d):
+    # For {'a': 3, 'b': 4, 'c': 9} -> {3: 'a', 4: 'b', 9: 'c'}
+    pass
+
+
 # def test_dict_reverse():
 #     assert dict_reverse({'a': 3, 'b': 4, 'c': 9}) == {3: 'a', 4: 'b', 9: 'c'}
+
+
+def atomic_weight(formula):
+
+    def weight(element='Na'):
+            return getattr(mendeleev, element).atomic_weight
+
+    pass
+
+
+# def test_atomic_weight():
+#     assert atomic_weight('NaCl')     == pytest.approx(58.4,  0.01)
+#     assert atomic_weight('CCl4')     == pytest.approx(153.8, 0.01)
+#     assert atomic_weight('H2O')      == pytest.approx(18.0,  0.01)
+#     assert atomic_weight('H2SO4')    == pytest.approx(98.1,  0.01)
+#     assert atomic_weight('C6H12COH') == pytest.approx(113.2, 0.01)
+
+
+def sequences(max_len):
+    '''
+    For a given max_len return all combinations of ACGT, first of
+    length 1, then 2, until max_len is reached
+    eg.
+        3 -> ['A', 'C', 'G', 'T', 'AC', 'AG', 'AT', 'CG', 'CT', 'GT', 'ACG',
+              'ACT', 'AGT', 'CGT']
+    '''
+    for n in range(1, max_len+1):
+        for el in itertools.combinations('ACGT', n):
+            yield ''.join(el)
+
+    #
+    # Now write tests for this fn
+    #
+
+
+def stock_prices(ticker):
+    prices = {
+        '2019-02-14': {
+            'MSFT':  106,
+            'GOOG': 1120,
+            # Missing NFLX
+        },
+        '2019-02-15': {
+            'MSFT':  108,
+            # Missing GOOG
+            'NFLX':  351,
+        },
+        '2019-02-18': {
+            'MSFT':  108,
+            'GOOG': 1119,
+            'NFLX':  352,
+        },
+        '2019-02-19': {
+            'MSFT':  109,
+            'GOOG': 1122,
+            'NFLX':  107,
+        },
+    }
+
+
+# def test_stock_prices():
+#     assert stock_prices('MSFT') = [ 106,  108,  108,  109]
+#     assert stock_prices('GOOG') = [1120, 1120, 1119, 1122]
+#     assert stock_prices('NFLX') = [None,  351,  352,  107]
 
 
 def main():
