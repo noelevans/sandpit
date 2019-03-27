6cfce26de72749f355a70de8b44fd6500b140c68
Noel Evans <noelevans@gmail.com>
2019-03-27 22:05:48 +0000

    Keep retrying until updated then email once new links are downloaded

--- economist_tools/fetch_economist.py
+++ economist_tools/fetch_economist.py
@@ -6,61 +6,74 @@ import time
 
 """ Download The Economist and send to kindle if new """
 
-def fetch():
+def fetch(existant_versions):
+    print('Fetching calibre recipe from Github')
     url = ('https://raw.githubusercontent.com/kovidgoyal/calibre/master/' +
            'recipes/economist.recipe')
-    cmd_1 = ['curl', url, '>', 'economist.recipe']
-    subprocess.run(cmd_1, check=True)
+    resp = requests.get(url)
 
-    cmd_2 = ['ebook-convert', 'economist.recipe', 'economist.mobi']
-    output = subprocess.run(cmd_2, stdout=subprocess.PIPE, check=True)
+    with open('economist.recipe', 'w') as f:
+        f.writelines(resp.text)
 
-    for line in output:
-        match = re.search(r'Got cover.*/(\d{8})', s)
+    print('Making mobi file')
+    cmd = 'ebook-convert economist.recipe economist.mobi'
+    proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
+    for line in iter(proc.stdout.readline, ""):
+        print(line, end='', flush=True)
+        # match = re.search(r'Got cover.*/(\d{8})', line)
+        match = re.search('Change course on Brexit or go', line)
         if match:
-            date = match.group()
-            break
+            # date = match.group()
+            date = '20190326'
+            if date in existant_versions:
+                print('Got version {} again'.format(date))
+                proc.terminate()
+                break
 
-    return 'economist.{}.mobi'.format(date)
+    proc.stdout.close()
+    proc.wait()
 
-
-def send_complex_message():
-    return requests.post(
-        'https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages',
-        auth=('api', 'YOUR_API_KEY'),
-        files=[('attachment', ('test.jpg', open('files/test.jpg','rb').read())),
-               ('attachment', ('test.txt', open('files/test.txt','rb').read()))],
-        data={'from': 'Excited User <YOU@YOUR_DOMAIN_NAME>',
-              'to': 'foo@example.com',
-              'cc': 'baz@example.com',
-              'bcc': 'bar@example.com',
-              'subject': 'Hello',
-              'text': 'Testing some Mailgun awesomness!',
-              'html': '<html>HTML version of the body</html>'})
+    date = '20190326'
+    filename = 'economist.{}.mobi'.format(date)
+    print('Generated {}'.format(filename))
+    return filename
 
 
-def main():
+def send_message(dated_filename):
+    '''
+    mail -A ~/repo/sandbox/economist_tools/economist.mobi -s "Email File 3: mail" -a "From: noelevans@gmx.co.uk" noelevans@gmail.com < /dev/null
+    '''
+    print('Emailing')
+    # cmd = ('mail -A {0} -s {0} -a From: noelevans@gmx.co.uk ' +
+    #        'noelevans@gmail.com < /dev/null').format(dated_filename)
+    # proc = subprocess.Popen(cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    # proc.communicate(input='')
+    subprocess.run(['./email.sh', dated_filename])
+
+
+def run():
     while True:
-        dated_filename = fetch()
+        existant_versions = [el.split('.')[1] for el in os.listdir()
+                             if el.endswith('.mobi')]
+        dated_filename = fetch(existant_versions)
 
         if os.path.exists(dated_filename):
             # The links are still being updated
             # Wait and retry in a few minutes
+            print('Retrying in 5 minutes')
             time.sleep(5 * 60)
         else:
             open(dated_filename, 'a').close()
-            subprocess.run([
-                'calibre-smtp', '-a', 'economist.mobi',
-                '-u', 'SENDERNAME@PROVIDER.com',
-                '-p', 'PASSWORD',
-                '-r', 'smtp.PROVIDER.com',
-                '--port', '587',
-                'SENDERNAME@PROVIDER.com',
-                'USERNAME@free.kindle.com'])
-            return
+            send_message(dated_filename)
+            print('Done.')
+            return dated_filename
 
         if time.localtime().tm_hour > 22:
             return
+
+
+def main():
+    run()
 
 
 if __name__ == '__main__':
