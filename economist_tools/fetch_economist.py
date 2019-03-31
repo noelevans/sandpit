d070ba5bd453631941dcd9372a8038540208a8bd
Noel Evans <noelevans@gmail.com>
2019-03-31 21:56:58 +0100

    Improve std out and remove hard-coding

--- economist_tools/fetch_economist.py
+++ economist_tools/fetch_economist.py
@@ -7,35 +7,36 @@ import time
 """ Download The Economist and send to kindle if new """
 
 def fetch(existant_versions):
-    print('Fetching calibre recipe from Github')
-    url = ('https://raw.githubusercontent.com/kovidgoyal/calibre/master/' +
-           'recipes/economist.recipe')
-    resp = requests.get(url)
+    # print('Fetching calibre recipe from Github')
+    # url = ('https://raw.githubusercontent.com/kovidgoyal/calibre/master/' +
+    #        'recipes/economist.recipe')
+    # resp = requests.get(url)
 
-    with open('economist.recipe', 'w') as f:
-        f.writelines(resp.text)
+    # with open('economist.recipe', 'w') as f:
+    #     f.writelines(resp.text)
 
     print('Making mobi file')
     cmd = 'ebook-convert economist.recipe economist.mobi'
-    proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, universal_newlines=True)
-    for line in iter(proc.stdout.readline, ""):
+    proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE,
+                            universal_newlines=True)
+    early_termination = False
+    for line in iter(proc.stdout.readline, ''):
         print(line, end='', flush=True)
-        # match = re.search(r'Got cover.*/(\d{8})', line)
-        match = re.search('Change course on Brexit or go', line)
+        match = re.search(r'Got cover.*print-covers/(\d{8})', line)
         if match:
-            # date = match.group()
-            date = '20190326'
+            date = match.groups()[0]
             if date in existant_versions:
                 print('Got version {} again'.format(date))
                 proc.terminate()
+                early_termination = True
                 break
 
     proc.stdout.close()
     proc.wait()
 
-    date = '20190326'
     filename = 'economist.{}.mobi'.format(date)
-    print('Generated {}'.format(filename))
+    if not early_termination:
+        print('Generated {}'.format(filename))
     return filename
 
 
@@ -44,10 +45,6 @@ def send_message(dated_filename):
     mail -A ~/repo/sandbox/economist_tools/economist.mobi -s "Email File 3: mail" -a "From: noelevans@gmx.co.uk" noelevans@gmail.com < /dev/null
     '''
     print('Emailing')
-    # cmd = ('mail -A {0} -s {0} -a From: noelevans@gmx.co.uk ' +
-    #        'noelevans@gmail.com < /dev/null').format(dated_filename)
-    # proc = subprocess.Popen(cmd.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    # proc.communicate(input='')
     subprocess.run(['./email.sh', dated_filename])
 
 
