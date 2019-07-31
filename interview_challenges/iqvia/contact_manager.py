48d11c621dcd1bb1d316406dab99c1e84c04a1cb
Noel Evans <noelevans@gmail.com>
2019-07-31 14:43:31 +0100

    All tests passing agnostic to user or email

--- interview_challenges/iqvia/contact_manager.py
+++ interview_challenges/iqvia/contact_manager.py
@@ -4,62 +4,53 @@ import redis
 
 
 app = Flask(__name__)
-rdb = redis.StrictRedis(db=0)
+rdb = redis.StrictRedis(
+    db=0, 
+    charset='utf-8', 
+    decode_responses=True)
 
 
-@app.route('/api/contact/<username>', methods=['GET'])
-def get_contact(username):
-    response = rdb.get('user:' + username)
+@app.route('/api/get/<key>', methods=['GET'])
+def get(key):
+    response = rdb.get(key)
     if not response:
-        return abort(404, 'User not found')
+        return abort(404, '{} not found'.format(key))
     return response
 
 
-@app.route('/api/contacts', methods=['GET'])
-def get_contacts():
-    keys = rdb.scan_iter('user:*') or []
-    contacts = [json.loads(rdb.get(k).decode('utf-8')) for k in keys]
-    return jsonify(contacts)
+@app.route('/api/get_all/<entity>', methods=['GET'])
+def get_all(entity):
+    keys = rdb.scan_iter('{}:*'.format(entity)) or []
+    entities = [json.loads(rdb.get(k)) for k in keys]
+    return jsonify(entities)
 
 
-@app.route('/api/contact/create', methods=['POST'])
-def create_contact():
-    if not request.json or 'username' not in request.json:
+@app.route('/api/create/<key>', methods=['POST'])
+def create(key):
+    if not request.json:
         abort(400, 'Bad request')
-    contact = json.dumps({
-        'username': request.json['username'],
-        'email': request.json['email'],
-        'first_name': request.json['first_name'],
-        'last_name': request.json['last_name']
-    })
-    rdb.set('user:' + request.json['username'], contact)
-    return contact, 201
+    value = json.dumps({
+        jk: request.json[jk] for jk in request.json.keys()})
+    rdb.set(key, value)
+    return value, 201
 
 
-@app.route('/api/contact/delete/<username>', methods=['DELETE'])
-def delete_contact(username):
-    count = rdb.delete(['user:' + username])
+@app.route('/api/delete/<key>', methods=['DELETE'])
+def delete_contact(key):
+    count = rdb.delete([key])
     if not count:
         return make_response(
-            jsonify({'error': 'Non-existant username'}),
-            409)
+            jsonify({'error': 'Non-existant key'}), 409)
     return json.dumps({'result': True})
 
 
-@app.route('/api/contact/update', methods=['PUT'])
-def update_contact():
-    if 'username' not in request.json:
-        abort(400, 'Missing username in update')
-    username = request.json['username']
-    old_data = json.loads(rdb.get('user:' + username))
-    contact = json.dumps({
-        'username': username,
-        'email': request.json.get('email', old_data['email']),
-        'first_name': request.json.get('first_name', old_data['first_name']),
-        'last_name': request.json.get('last_name', old_data['last_name'])
-    })
-    rdb.set('user:' + username, contact)
-    return contact
+@app.route('/api/update/<key>', methods=['PUT'])
+def update(key):
+    old_value = json.loads(rdb.get(key))
+    value = json.dumps({
+        jk: request.json[jk] for jk in request.json.keys()})
+    rdb.set(key, value)
+    return value
 
 
 if __name__ == '__main__':
