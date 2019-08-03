c2a67de7b3f156ab172519999613c45fe8b29260
Noel Evans <noelevans@gmail.com>
2019-08-03 20:09:00 +0100

    Make some sample users

---
+++ interview_challenges/iqvia/example_commands.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+
+# rm -f contact_manager.db
+
+curl -i -H "Content-Type: application/json" -X POST -d '{"username": "noel", "first_name": "Neil", "last_name":"Evans"}' http://localhost:5000/api/create/contact
+curl -i -H "Content-Type: application/json" -X POST -d '{"username": "kam", "first_name": "Cam", "last_name":"Ifans"}' http://localhost:5000/api/create/contact
+
+curl -i -H "Content-Type: application/json" -X PUT -d '{"first_name": "Noel"}' http://localhost:5000/api/update/contact/noel
+curl -i -H "Content-Type: application/json" -X PUT -d '{"first_name": "Kam", "last_name": "Evans"}' http://localhost:5000/api/update/contact/kam
+
+curl -i -H "Content-Type: application/json" -X DELETE http://localhost:5000/api/delete/contact/noel
+
