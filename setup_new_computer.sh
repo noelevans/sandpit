b2256e0dc32fc580ec980f9cc87b17326d70c6c7
Noel Evans <noelevans@gmail.com>
2019-08-06 20:29:42 +0100

    New things for new machine

--- setup_new_computer.sh
+++ setup_new_computer.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+mkdir ~/.vim/swap
+mkdir ~/.vim/backup
+mkdir ~/.vim/undo
+
 mkdir ~/repo/
 (cd /home/repo/; git clone https://github.com/noelevans/sandpit.git)
 
 ln -s ~/repo/sandpit/pylint ~/.pylintrc
 ln -s ~/repo/sandpit/start_ipython.py  ~/.ipython/profile_default/startup/start_ipython.py
 ln -s ~/repo/sandpit/matplotlibrc .config/matplotlib/matplotlibrc
+ln -s ~/repo/sandpit/.vimrc ~/.vimrc
+ln -s ~/repo/sandpit/.gitconfig ~/.gitconfig
+ln -s ~/repo/sandpit/.bashrc ~/.bashrc
