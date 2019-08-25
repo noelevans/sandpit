bedccfb058738725cff5b128963dc407ebd8349f
Noel Evans <noelevans@gmail.com>
2019-08-25 21:24:05 +0100

    Cleaner git check for more accurate grep-ing

--- backup_work.sh
+++ backup_work.sh
@@ -2,5 +2,5 @@
 
 
 mkdir -p ../store/`date +%Y%m%d%H%M`
-files=$(git status | egrep "py|sql|sh|txt" | sed "s/modified://")
-echo "$files" | xargs -I{} cp "{}" ../store/`date +%Y%m%d%H%M`/
+files=$(git status -s | cut -b4-)
+echo "$files" | xargs -I{} cp -r "{}" ../store/`date +%Y%m%d%H%M`/
