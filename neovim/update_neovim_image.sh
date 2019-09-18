fd78f64c6d2deb53ca3d27815f442b627fdfa876
Noel Evans <noelevans@gmail.com>
2019-09-18 22:15:33 +0100

    Be agnostic to username

--- neovim/update_neovim_image.sh
+++ neovim/update_neovim_image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir -p /home/noel/repo/neovim-image/
-curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > /home/noel/repo/neovim-image/nvim.appimage
-chmod +x /home/noel/repo/neovim-image/nvim.appimage
+mkdir -p ~/repo/neovim-image/
+curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > ~/repo/neovim-image/nvim.appimage
+chmod +x ~/repo/neovim-image/nvim.appimage
