32e1eed386f705946aea414719f9d23337cefc11
Noel Evans <noelevans@gmail.com>
2019-09-20 13:52:47 +0100

    Deal with different username and coc dependencies

--- neovim/update_neovim_image.sh
+++ neovim/update_neovim_image.sh
@@ -3,3 +3,21 @@
 mkdir -p ~/repo/neovim-image/
 curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > ~/repo/neovim-image/nvim.appimage
 chmod +x ~/repo/neovim-image/nvim.appimage
+
+# Necessary for coc.nvim plugin
+if ! [ -x "$(command -v nodejs)" ]; then
+    sudo apt-get install nodejs
+fi
+
+# Necessary for coc.nvim plugin
+if ! [ -x "$(command -v yarn)" ]; then
+    curl --compressed -o- -L https://yarnpkg.com/install.sh | bash
+fi
+
+# .bashrc change
+if ! [ -x "$(command -v vim | grep nvim)" ]; then
+    read -p "Add vim <= neovim alias? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo 'alias vim=~/repo/neovim-image/nvim.appimage' >> ~/.bashrc
+    fi
+fi
