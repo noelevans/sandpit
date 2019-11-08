00919584f5300d336a838e8ec6d06d82e151a889
Noel Evans <noelevans@gmail.com>
2019-11-08 21:18:08 +0000

    Keep the scriptin hot

--- backup_work.sh
+++ backup_work.sh
@@ -3,4 +3,4 @@
 
 mkdir -p ../store/`date +%Y%m%d%H%M`
 files=$(git status -s | cut -b4-)
-echo "$files" | xargs -I{} cp -r "{}" ../store/`date +%Y%m%d%H%M`/
+echo "$files" | xargs -I{} cp -r --parents "{}" ../store/`date +%Y%m%d%H%M`/
