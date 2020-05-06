3fdaa84b6e2931ac99bb79ec12c6a15f436e656d
Noel Evans <noelevans@gmail.com>
2020-05-06 21:43:39 +0100

    First changes on ubuntu 20.04

--- setup_new_computer.sh
+++ setup_new_computer.sh
@@ -4,6 +4,7 @@ mkdir ~/.vim
 mkdir ~/.vim/swap
 mkdir ~/.vim/backup
 mkdir ~/.vim/undo
+mkdir -p ~/.config/nvim
 
 mkdir ~/repo/
 (cd /home/repo/; git clone https://github.com/noelevans/sandpit.git)
@@ -12,6 +13,7 @@ ln -s ~/repo/sandpit/pylint ~/.pylintrc
 ln -s ~/repo/sandpit/start_ipython.py  ~/.ipython/profile_default/startup/start_ipython.py
 ln -s ~/repo/sandpit/matplotlibrc .config/matplotlib/matplotlibrc
 ln -s ~/repo/sandpit/.vimrc ~/.vimrc
+ln -s ~/repo/sandpit/.vimrc ~/.config/nvim/init.vim
 ln -s ~/repo/sandpit/.gitconfig ~/.gitconfig
 ln -s ~/repo/sandpit/.bashrc ~/.bashrc
 ln -s ~/repo/sandpit/.cocnvimrc ~/.cocnvimrc
---
+++ ubuntu-installs.sh
@@ -0,0 +1,9 @@
+sudo apt-get install python3-venv
+sudo apt-get install python3-pip
+sudo apt-get install python2.7
+sudo apt-get install python
+sudo apt-get install ack
+sudo apt-get install arp-scan
+sudo apt-get install nmap
+
+pip3 install pandas
