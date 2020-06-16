ee848b88577dfb522ee434e2af1092ceaa271842
Noel Evans <noelevans@gmail.com>
2020-06-16 15:46:52 +0100

    Ready for dotfiles repo

--- file_fixer.sh
+++ file_fixer.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+# To use this with pre-commit rather than the employer's
+# pre-commit, do:
+#
+#     pre-commit install -c .pre-commit-noel.yaml
+#
+# which will then reference this script
 
 # Remove pdb uses
 if grep -q "import .*pdb" $1; then
+    echo 'Removing pdb import statements in' $1
     sed -i '/.*import .*pdb/d' $1
 fi
 if grep -q "pdb.set_trace" $1; then
+    echo 'Removing pdb set_trace statements in' $1
     sed -i '/.*pdb.set_trace/d' $1
 fi
 
 # Get rid of unused imports
 if ! autoflake --check $1; then
+    echo 'Organising imports in' $1
     autoflake --in-place $1
 fi
 
 # Format module
 if ! black --check $1; then
+    echo 'Black formatting' $1
     black $1
 fi
 
