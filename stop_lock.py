91f71c5c573d92a237fcea3c92d91fd5372b8246
Noel Evans <noelevans@gmail.com>
2020-12-10 21:10:47 +0000

    Improve mouse wiggles

--- stop_lock.py
+++ stop_lock.py
@@ -3,6 +3,9 @@ import time
 import win32api
 import win32con
 
+from pynput.mouse import Listener as MouseListener
+from pynput.keyboard import Listener as KeyboardListener
+
 
 """
 Simulate keyboard actions to prevent a screensaver /
@@ -10,7 +13,15 @@ screenlock from activating on a computer
 """
 
 now = datetime.datetime.now
+
+last_move = now()
 start_time = now()
+
+
+def on_move(x, y):
+    last_move = now()
+    print('m')
+
 
 def act():
     win32api.keybd_event(0x28, 0, 0, 0)
@@ -20,10 +31,14 @@ def act():
 
 
 def main():
+    with MouseListener( on_move=on_move,) as mouse_listener:
+        mouse_listener.join()
+    with KeyboardListener( on_move=on_move,) as keyboard_listener:
+        keyboard_listener.join()
     while now() < start_time + datetime.timedelta(hours=16):
-        for _ in range(3):
+        if (now() - last_move).seconds > 120:
             act()
-        print('.')
+            print('.')
 
         time.sleep(20)
 
