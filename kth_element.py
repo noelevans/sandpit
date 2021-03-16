d4b8726a0eec04fdbf3a4826b5fa835b790e7164
Noel Evans <noelevans@gmail.com>
2021-03-16 09:37:10 +0000

    Further comparison of 2n algorithm vs alternatives

--- kth_element.py
+++ kth_element.py
@@ -1,5 +1,7 @@
 import random
 import pytest
+import statistics
+import time
 
 """
 You have a list of 10M values. You want to order all the values to get the
@@ -21,11 +23,31 @@ def get(ul, k):
         else:
             aft.append(el)
     if k < len(fore):
-        return get(fore, k, 0)
+        return get(fore, k)
     elif k == len(fore):
         return pivot
     else:
         return get(aft, k - len(fore) - 1)
+
+
+def _sorted(ul):
+    if not ul:
+        return []
+    return (
+        _sorted([el for el in ul[1:] if el < ul[0]])
+        + [ul[0]]
+        + _sorted([el for el in ul[1:] if el >= ul[0]])
+    )
+
+
+def c_traditional_get(ul, k):
+    ol = _sorted(ul)
+    return ol[k]
+
+
+def native_traditional_get(ul, k):
+    ol = _sorted(ul)
+    return ol[k]
 
 
 @pytest.mark.parametrize("arg, expected", [(5, 106), (95, 196)])
@@ -34,3 +56,25 @@ def test_kth_element(arg, expected):
     random.seed(0)
     random.shuffle(ul)
     assert get(ul, arg) == expected
+
+
+if __name__ == "__main__":
+
+    def _timer(fn, ul):
+        index = len(ul) // 2
+        start = time.time()
+        _ = fn(ul, index)
+        return time.time() - start
+
+    for e in range(1, 8):
+        n = 10 ** e
+        ul = list(range(10 ** e))
+        random.shuffle(ul)
+
+        two_n = _timer(get, ul)
+        native = _timer(native_traditional_get, ul)
+        c_native = _timer(c_traditional_get, ul)
+        stats_lib = _timer(lambda the_ul, x: statistics.median(the_ul), ul)
+        print(
+            f"For {len(ul)} records: 2n = {two_n}, native = {native}, c_native = {c_native}, stats_lib = {stats_lib}"
+        )
